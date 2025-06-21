c3e0082bc0819eb96f5e0f2c39e9094ab11cce2e
Bae Hyeonji <wns038200@naver.com>
2025-06-22 02:36:33 +0900

    feat: 빛의 굴절 화면 구현

---
+++ RefractionForm/DoubleBufferedPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhysicsPlayground
+{
+    class DoubleBufferedPanel : Panel
+    {
+        public DoubleBufferedPanel()
+        {
+            this.DoubleBuffered = true;
+            this.ResizeRedraw = true;
+        }
+    }
+}
