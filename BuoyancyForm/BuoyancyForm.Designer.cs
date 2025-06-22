6ae003e4089141aaa6828950aabb7bbe8f722eed
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-06-22 11:19:40 +0900

    ok

--- BuoyancyForm/BuoyancyForm.Designer.cs
+++ BuoyancyForm/BuoyancyForm.Designer.cs
@@ -28,7 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnBack = new System.Windows.Forms.Button();
+            this.timeRestart = new System.Windows.Forms.Button();
+            this.timeStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBack
@@ -36,27 +45,99 @@
             this.btnBack.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.btnBack.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btnBack.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.btnBack.Location = new System.Drawing.Point(26, 26);
+            this.btnBack.Location = new System.Drawing.Point(23, 21);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(121, 49);
+            this.btnBack.Size = new System.Drawing.Size(106, 39);
             this.btnBack.TabIndex = 0;
             this.btnBack.Text = "<- 뒤로가기";
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // timeRestart
+            // 
+            this.timeRestart.Location = new System.Drawing.Point(695, 21);
+            this.timeRestart.Name = "timeRestart";
+            this.timeRestart.Size = new System.Drawing.Size(75, 23);
+            this.timeRestart.TabIndex = 3;
+            this.timeRestart.Text = "재시작";
+            this.timeRestart.UseVisualStyleBackColor = true;
+            this.timeRestart.Click += new System.EventHandler(this.timeRestart_Click);
+            // 
+            // timeStop
+            // 
+            this.timeStop.Location = new System.Drawing.Point(695, 58);
+            this.timeStop.Name = "timeStop";
+            this.timeStop.Size = new System.Drawing.Size(75, 23);
+            this.timeStop.TabIndex = 4;
+            this.timeStop.Text = "일시정지";
+            this.timeStop.UseVisualStyleBackColor = true;
+            this.timeStop.Click += new System.EventHandler(this.timeStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(693, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "나무상자를 클릭하여\r\n조종 할 수 있습니다.\r\n";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 256);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(875, 305);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PhysicsPlayground.Properties.Resources.New_Piskel__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(305, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
             // BuoyancyForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeStop);
+            this.Controls.Add(this.timeRestart);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnBack);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "BuoyancyForm";
-            this.Size = new System.Drawing.Size(1000, 700);
+            this.Size = new System.Drawing.Size(875, 561);
+            this.Load += new System.EventHandler(this.BuoyancyForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button timeRestart;
+        private System.Windows.Forms.Button timeStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- BuoyancyForm/BuoyancyForm.cs
+++ BuoyancyForm/BuoyancyForm.cs
@@ -15,6 +15,11 @@ namespace PhysicsPlayground
         public BuoyancyForm()
         {
             InitializeComponent();
+
+
+
+            pictureBox2.SendToBack();
+            timer1.Start();
         }
 
         private void btnBack_Click(object sender, EventArgs e)
@@ -26,6 +31,141 @@ namespace PhysicsPlayground
 
                 mForm.LoadMainMenu();
             }
+        }
+
+
+        private void BuoyancyForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timeRestart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private float force = 0;
+        private static float speed = 0;
+        private const float gravity = -0.5f;
+        private static float position = 0;
+
+        private static float delta_time = 0.3f;
+
+        private const float line = 300;
+        private const float friction_공기 = 0.01f;
+        private const float friction_물 = 0.1f;
+        private const float _V = 2f;
+        private const float size = 30;
+
+        private bool onClick;
+        private float click_force = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (position < 0 && speed < 0)
+            {
+                speed *= -1;
+            }
+
+            force = 0;
+
+            force += gravity;
+            force += Force_Click();
+            force += Force_buoyancy();
+            force += speed * Friction();
+
+            
+
+            
+
+            speed += force * delta_time;
+            position = position + speed * delta_time;
+
+           // Print();
+
+            pictureBox1.Location = new Point(pictureBox1.Location.X, 500 - (int)position);
+        }
+
+        private float Force_Click()
+        {
+            if (!onClick) return 0;
+
+            float mouse_pos = 500 - PointToClient(new Point(Control.MousePosition.X, Control.MousePosition.Y)).Y;
+
+            if (mouse_pos - position > 100) return 1;
+            else if(mouse_pos - position < 100) return -1;
+            else
+            {
+                return (mouse_pos - position) / 100;
+            }
+
+
+            
+        }
+
+
+        private float Force_buoyancy()
+        {
+            float d = line - position;
+            float force = 0;
+
+            if (d > size / 2)
+            {
+                force = _V;
+            }
+            else if (d < size / 2)
+            {
+
+            }
+            else
+            {
+                force = _V * (d + size / 2);
+            }
+
+            return force;
+        }
+
+
+
+        private float Friction()
+        {
+            float friction;
+            if (position + size / 2 < line)
+            {
+                friction = friction_물;
+            }
+            else if (position - size / 2 > line)
+            {
+                friction = friction_공기;
+            }
+            else if (speed > 0)
+            {
+                friction = friction_공기;
+            }
+            else
+            {
+                friction = friction_물;
+            }
+
+            return -friction;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            onClick = true;
+            pictureBox1.Image = global::PhysicsPlayground.Properties.Resources.New_Piskel__4_;
+        }
+
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            onClick = false;
+            pictureBox1.Image = global::PhysicsPlayground.Properties.Resources.New_Piskel__2_;
         }
     }
 }
